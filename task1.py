d6562706f291e0d8a21deb900ab1a2ca55558742
Nalini Rangamani <nalini.rangamani@gmail.com>
2017-12-11 20:46:07 +0530

    adding  files

---
+++ task1.py
@@ -0,0 +1,84 @@
+from flask import Flask, request,redirect,send_file
+from flask import make_response
+from flask import render_template
+import requests
+import json
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return "Hello World - Nalini"
+
+@app.route('/authors')
+def proxy_example():
+#for author id and number of posts
+	posts = requests.get("https://jsonplaceholder.typicode.com/posts")
+	posts_data=posts.json()
+#get author name and other authordetails
+	author = requests.get("https://jsonplaceholder.typicode.com/users")
+	author_data=author.json()
+	retStr=""
+	Userid_old = posts_data[0]["userId"]
+	Number_of_posts=0
+	j=0
+	for i in range(len(posts_data)):
+		if posts_data[i]["userId"]==Userid_old:
+			Number_of_posts = Number_of_posts + 1
+		else:
+			if Userid_old==author_data[j]["id"]:
+#get author name
+				retStr += author_data[j]["name"] + " " + str(Number_of_posts) + "<br>"
+				j=j+1
+			Number_of_posts=1
+			Userid_old=posts_data[i]["userId"]			
+	retStr +=  author_data[j]["name"] + " " + str(Number_of_posts) + "\r\n"
+	return retStr
+
+#set cookies with my name and age
+@app.route('/setcookie')
+def setcookie():
+	resp = make_response('Setting both the cookies !!')
+	resp.set_cookie('name', 'Nalini')
+	resp.set_cookie('age',str(10))
+	return resp 
+ 
+#get cokies both name and age and display them on the browser
+@app.route('/getcookies')
+def getcookie():
+	ret_name = request.cookies.get('name')
+	ret_age = request.cookies.get('age')
+	return 'Name is ' + ret_name + ' Age is ' + ret_age 
+
+#deny request
+@app.route('/robots.txt')
+def deny_req():
+	return redirect("http://httpbin.org/deny", code=302)
+
+#render a html page
+@app.route('/html')
+def display_html():
+	return render_template("SimpleHtml.htm")
+
+#display an image
+@app.route('/image')
+def display_image():
+	return send_file("SimpleImage.jpeg",mimetype='image/gif')
+
+#displays a textbox witha submit button
+@app.route('/input')
+def display_get_text_form():
+	return render_template('Get_text_form.html')
+
+#display the text entered by the user on stdout
+@app.route('/input', methods=['POST'])
+def display_text():
+	text = request.form['text']
+	print(text)
+	return ""
+
+if __name__ == "__main__":
+	app.run()
+	
